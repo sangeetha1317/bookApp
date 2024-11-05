6adec1f3344f381a6f7f46f7a1c80c5cc2599aa3
Sangeetha Thyagarajan <sangeethathyagarajan13@gmail.com>
2024-11-05 16:05:48 -0500

    Books list and detail screen

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+const Stack = createStackNavigator();
+
+function HomeStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="BooksList" component={BooksListScreen} />
+            <Stack.Screen name="BookDetail" component={BookDetailScreen} />
+        </Stack.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeStack} />
+                <Tab.Screen name="Borrowed" component={BorrowedScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
